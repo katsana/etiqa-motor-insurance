0a702cbb7a94cf155eb8f9c85df78004d08ee0ab
Mior Muhammad Zaki <crynobone@gmail.com>
2018-03-23 21:10:14 +0800

    Improves tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/ClientTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Etiqa\MotorInsurance\TestCase;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Etiqa\MotorInsurance\Client;
+use Laravie\Codex\Testing\FakeRequest;
+
+class ClientTest extends TestCase
+{
+    /**
+     * Teardown the test environment.
+     */
+    protected function tearDown(): void
+    {
+        m::close();
+    }
+
+    /** @test */
+    public function it_has_proper_signature()
+    {
+        $faker = FakeRequest::create();
+
+        $client = new Client($faker->http(), 'id', 'secret');
+
+        $this->assertSame('id', $client->getClientId());
+        $this->assertSame('secret', $client->getClientSecret());
+
+        $this->assertNull($client->getAccessToken());
+    }
+
+    /** @test */
+    public function it_can_use_custom_passport_endpoint()
+    {
+        $faker = FakeRequest::create();
+
+        $client = new Client($faker->http(), 'id', 'secret');
+
+        $client->useCustomPassportEndpoint('https://api.etiqa.com.my/oauth');
+
+        $this->assertSame('https://api.etiqa.com.my/oauth', $client->getPassportEndpoint());
+    }
+
+    /** @test */
+    public function it_can_set_access_token()
+    {
+        $faker = FakeRequest::create();
+
+        $client = new Client($faker->http(), 'id', 'secret');
+
+        $this->assertNull($client->getAccessToken());
+
+        $client->setAccessToken('abc');
+
+        $this->assertSame('abc', $client->getAccessToken());
+    }
+}
--- tests/One/QuoteTest.php
+++ tests/One/QuoteTest.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Etiqa\MotorInsurance\TestCase;
+namespace Etiqa\MotorInsurance\TestCase\One;
 
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
 use Etiqa\MotorInsurance\Client;
 use Laravie\Codex\Testing\FakeRequest;
 
-class RequestTest extends TestCase
+class QuoteTest extends TestCase
 {
     /**
      * Teardown the test environment.
@@ -24,15 +24,32 @@ class RequestTest extends TestCase
      */
     public function it_can_throws_exception_when_status_is_errors()
     {
-        $body = '{"coverage_type":"MT","id_type":"1","id_value":"820101015510","vehicle_postcode":"50000","vehicle_reg_no":"ABC123","drivers":[{"email":"demo.etiqa@gmail.com"}],"agent_code":"agent","operator_code":"operator"}';
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer abc',
+        ];
+
+        $payload = [
+            'coverage_type' => 'MT',
+            'id_type' => '1',
+            'id_value' => '820101015510',
+            'vehicle_postcode' => '50000',
+            'vehicle_reg_no' => 'ABC123',
+            'drivers' => [
+                ['email' => 'demo.etiqa@gmail.com'],
+            ],
+            'agent_code' => 'agent',
+            'operator_code' => 'operator',
+        ];
 
         $faker = FakeRequest::create()
-                    ->call('POST', m::type('Array'), $body)
+                    ->call('POST', $headers, json_encode($payload))
                     ->expectEndpointIs('/api/v1.0/my/insurance/motor/quote')
                     ->shouldResponseWith(200, '{"code":"A038","message":"Driver record incomplete. Please provide details","status":"ERROR","data":null}');
 
         $client = new Client($faker->http(), 'id', 'secret');
+        $client->setAccessToken('abc');
 
-        $client->uses('Quote')->submit(json_decode($body, true));
+        $client->uses('Quote')->submit($payload);
     }
 }
